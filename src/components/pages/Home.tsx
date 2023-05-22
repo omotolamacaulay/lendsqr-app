776c699eddfd5c55404733328dfd90e6fd3981eb
Omotola Macaulay <tolamacaulay@gmail.com>
2023-05-22 22:05:17 +0100

    Add hamburger and implement sidenav toggle

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,19 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 import Sidenav from "../layout/Sidenav"
 import Navbar from "../layout/Navbar"
 import '../styles/navbar.scss'
 import '../styles/home.scss'
+import { Turn as Hamburger } from 'hamburger-react'
 import { UserProvider } from "../UserProvider"
 
 const Home = ({ children }: { children: React.ReactElement }) => {
+    const [collapsed, setCollapsed] = useState(true)
     return (
         <UserProvider>
-            <div className="container">
+            <div className={!collapsed ? '' : "container"} style={{ padding: 16 }}>
                 <Navbar />
+                <div className="hamburger-toggle">
+                    <Hamburger size={20} toggled={collapsed} toggle={setCollapsed} />
+                </div>
                 <div>
                     {children}
                 </div>
-                <Sidenav />
+                {collapsed && <Sidenav name='sidebar' />}
             </div>
         </UserProvider>
     )
