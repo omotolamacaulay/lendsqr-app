295b7968df9075fb3283b0aad440c6fbc7fd2f99
Omotola Macaulay <tolamacaulay@gmail.com>
2023-05-22 21:50:10 +0100

    Implement tooltip and install antd

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from 'react';
 import { UserTile } from "./UserTile";
 import { useUsers } from "../UserProvider";
 import '../styles/users.scss'
 import { DashUsers, DashActiveUsers, DashUsersWithLoans, DashUsersWithSavings, Filter } from "../../assets";
-// import UserDetail from "./UserDetail";
+import FilterUserForm from './FilterForm';
 
 
 const Users = () => {
     const { users } = useUsers();
+    const [collapsed, setCollapsed] = useState(false)
     return (
         <div className="home">
             <h2>Users</h2>
@@ -31,7 +32,7 @@ const Users = () => {
                         </span>
                     </div>
                     <div className="dashboard__summary--box__number">
-                        <span> 22</span>
+                        <span>{users.length}</span>
                     </div>
                 </article>
                 <article className="dashboard__summary--box">
@@ -53,31 +54,36 @@ const Users = () => {
                         </span>
                     </div>
                     <div className="dashboard__summary--box__number">
-                        <span> 22</span>
+                        <span>{users.length}</span>
                     </div>
                 </article>
             </div>
             <div className="card-group-2">
                 <div className="header-body">
-                <div className="table-head">
-                    <div className="group-1">
-                        <div>organization <img src={Filter()} alt="Filter Organization" /></div>
-                        <div>Username<img src={Filter()} alt="Filter Username" /></div>
-                        <div>Email<img src={Filter()} alt="Filter Email" /></div>
-                        <div>Phone number<img src={Filter()} alt="Filter Phone Number" /></div>
-                        <div>Date joined<img src={Filter()} alt="Filter Date Joined" /></div>
+                    <div className="table-head">
+                        <div className="group-1">
+                            <div>organization <img src={Filter()} alt="Filter Organization" onClick={() => setCollapsed(!collapsed)} /></div>
+                            <div>Username<img src={Filter()} alt="Filter Username" onClick={() => setCollapsed(!collapsed)} /></div>
+                            <div>Email<img src={Filter()} alt="Filter Email" onClick={() => setCollapsed(!collapsed)} /></div>
+                            <div>Phone number<img src={Filter()} alt="Filter Phone Number" onClick={() => setCollapsed(!collapsed)} /></div>
+                            <div>Date joined<img src={Filter()} alt="Filter Date Joined" onClick={() => setCollapsed(!collapsed)} /></div>
 
+                        </div>
+                        <div className="group-2">
+                            <div>Status<img src={Filter()} alt="Filter Status" /></div>
+                            <div></div>
+                        </div>
                     </div>
-                    <div className="group-2">
-                        <div>Status<img src={Filter()} alt="Filter Status" /></div>
-                        <div></div>
-                    </div>
-                </div>
-                {users.map(user => (
-                    <UserTile key={user.id} user={user} />
-                ))}
+                    {users.map(user => (
+                        <UserTile key={user.id} user={user} />
+                    ))}
 
                 </div>
+                {/*                 
+                <UserPagination total={users.length}  /> */}
+                {collapsed && <FilterUserForm onSubmit={() => { }} />}
+
+
             </div>
         </div>
     )
